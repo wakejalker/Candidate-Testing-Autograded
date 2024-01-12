8762b993ab9f5e931dbc139010e0b40b14ce93bb
Jake Walker <jake@Jakes-MBP.attlocal.net>
2024-01-12 00:56:42 -0600

    got GA P1 mostly working. need sleep so will troubleshoot more in AM or ask for help

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,18 +18,28 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Please enter your name. ");
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+candidateAnswer = input.question("Question 1: " + question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer === correctAnswer) {
+  console.log("Correct!");
+} else (candidateAnswer != correctAnswer); {
+  console.log("Incorrect.");
+} 
+//why are the parentheses a different color than usual...not sure why "else" is unexpected either
+// GOT IT. had my semi colon in the wrong spot 
+// attempt 1: oop not right, both displayed lmao 
+// attempt 2: adding else if didn't help hrm
+// atttempt 3: i think i had it coded incorrectly, updated from !correctAnswer. Still logs both when answered correctly, but CORRECTLY logs incorrect when answered in correctly. whyyy
 
 
 
@@ -42,7 +52,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello, " + candidateName + ".");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
